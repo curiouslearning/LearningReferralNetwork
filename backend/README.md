580a16b7d840b2f8417752d79a0642ab0ced9a2a
Johnny Yip <jcyip@google.com>
2020-03-24 16:04:29 -0700

    Query firestore through express app
    Signed-off-by: Johnny Yip <jcyip@google.com>

---
+++ backend/README.md
@@ -0,0 +1,20 @@
+# Learning Referral Network Backend
+This web app demonstrates the use of Firebase Cloud Functions to serve app referrals
+
+## Development Environment
+1. Install Node.js from the [official website](https://nodejs.org/en/)
+2. Install Firebase CLI tools
+```
+npm install -g firebase-tools
+```
+3. Login to Firebase
+```
+firebase login
+```
+4. Deploy
+```
+firebase deploy --only functions
+```
+
+## API
+Swagger API file in api.json
--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,15 +1,39 @@
-const functions = require('firebase-functions');
 const port = 3000
+
+// Firebase-admin is used to access Firebase
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+process.env.GCLOUD_PROJECT = 'learning-referral-network';
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const db = admin.firestore();
 
 var Express = require('express');
 var App = Express();
 
-App.listen(port, function() {
-    console.log("Listening to port " + port);
+App.listen(port, function () {
+  console.log("Listening to port " + port);
 });
 
-App.get('/ping', (request, response) => {
- response.send("Hello from Firebase!");
+App.get('/app/:id', (req, res) => {
+  var appsRef = db.collection('apps');
+
+  appsRef.where('appId', '==', parseInt(req.params.id)).get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          res.status(200).send({});
+          return null;
+        }
+        return snapshot.forEach(doc => {
+          console.log(doc.id, '=>', doc.data().appId);
+          res.status(200).send(doc.data());
+        });
+      })
+      .catch(err => {
+        console.log('error getting app', err);
+        res.status(500).send("System error, please try again.");
+      })
 });
 
-exports.app = functions.https.onRequest(App);
+exports.admin = functions.https.onRequest(App);
